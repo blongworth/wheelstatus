fe5c95b31bc16569b54709fb26ace1b120c6948e
blongworth <blongworth@gmail.com>
2014-10-24 16:44:51 -0400

    Figuring out populating select

--- server.R
+++ server.R
@@ -7,10 +7,28 @@
 
 library(shiny)
 
+usamspath = "H:/USAMS/Results"
+cfamspath = "H:/CFAMS/Results"
+system = 1
+
+#Get available wheels
+
+if (system == 1) {
+  wheelpath = usamspath
+} else if (system == 2) {
+  wheelpath = cfamspath
+} else {
+  #Stop
+}
+
+wheels = list.files(path = wheelpath, pattern = "*AMS*.*")
+
 shinyServer(function(input, output) {
 
+  
   output$distPlot <- renderPlot({
 
+  
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
@@ -20,4 +38,31 @@ shinyServer(function(input, output) {
 
   })
 
+})
+
+library(shiny)
+library(datasets)
+
+# Define server logic required to summarize and view the 
+# selected dataset
+shinyServer(function(input, output) {
+  
+  # Return the requested dataset
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars" = cars)
+  })
+  
+  # Generate a summary of the dataset
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  # Show the first "n" observations
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
 })
