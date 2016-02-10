e580a90032c5313e91e22b36bba6573be7aaa217
Brett Longworth <blongworth@gmail.com>
2016-02-10 16:21:22 -0500

    Use functions from amstools.

--- wstat.R
+++ wstat.R
@@ -2,26 +2,20 @@
 # quick wheel status from cmd line
 
 
-
-#load lobraries
-suppressPackageStartupMessages(library(stats))
+#load libraries
 suppressPackageStartupMessages(library(dplyr))
-
-#source functions
-source("./wheelfuncs.R")
+suppressPackageStartupMessages(library(amstools))
 
 #directories
 usamspath <- "/mnt/shared/USAMS/Results"
-
-#load most recent file
 
 #Get and order wheelnames by system
 details <- file.info(list.files(path = usamspath, pattern = "*AMS*.*",
                                 full.names=TRUE))
 details <- details[with(details, order(as.POSIXct(mtime))), ]
 wheels <- rownames(details)
-wheel <- wheels[1]
-data <- readCFWheel(wheel)
+wheel <- tail(wheels, 1)
+data <- mungeResfile(readResfile(wheel))
 
 #last run
 print(select(tail(data), ts, Pos, Meas, Sample.Name, he12C, cor1412he))
@@ -33,3 +27,4 @@ sum  <- data %>% filter(Num == "S") %>%
   summarise_each(funs(mean, sd, rsd)) 
 
 print(sum)
+
