b710bca15646bb093041be32b66ea6352eab20c3
Brett Longworth <blongworth@gmail.com>
2016-03-11 14:42:56 -0500

    ignore OX-II primaries

--- wstat.R
+++ wstat.R
@@ -24,7 +24,7 @@ print(select(tail(data), ts, Pos, Meas, Sample.Name, he12C, cor1412he))
 
 #current stats
 
-sum  <- data %>% filter(Num == "S") %>% 
+sum  <- data %>% filter(Num == "S", grepl("OX-I$", Sample.Name)) %>% 
   select(X14.12he, cor1412he) %>% 
   summarise_each(funs(mean, sd, rsd)) 
 
